edc246df8c99c1d994629adff882679d783b5392
alvaro <alvarocontrino@hotmail.com>
2025-07-14 18:42:52 -0500

    cambio3

---
+++ src/app/components/carrusel/carrusel.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel',
+  standalone: true, // <-- Agrega esta línea
+  imports: [],
+  templateUrl: './carrusel.html',
+  styleUrl: './carrusel.css'
+})
+export class Carrusel {
+
+}
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Carrusel } from '../../components/carrusel/carrusel';
+
+@Component({
+  selector: 'app-home',
+  imports: [Carrusel],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
