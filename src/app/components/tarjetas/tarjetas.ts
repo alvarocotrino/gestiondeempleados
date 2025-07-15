2d64ac62d013dbb62f04f811f7c8b0fde31abaf4
alvaro <alvarocontrino@hotmail.com>
2025-07-15 13:41:51 -0500

    cambio 3.1

---
+++ src/app/components/tarjetas/tarjetas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjetas',
+  imports: [Tarjetas],
+  templateUrl: './tarjetas.html',
+  styleUrl: './tarjetas.css'
+})
+export class Tarjetas {
+
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Carrusel } from '../../components/carrusel/carrusel';
+import { Tarjetas } from '../../components/tarjetas/tarjetas';
 
 @Component({
   selector: 'app-home',
-  imports: [Carrusel],
+  imports: [Carrusel,Tarjetas],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
