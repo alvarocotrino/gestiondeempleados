bf92798e0ab1a8e81971436be59cfa27eb55258b
alvaro <alvarocontrino@hotmail.com>
2025-07-16 22:12:15 -0500

    cambio3.3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { EmpleadoList } from './pages/empleado-list/empleado-list';
 
 export const routes: Routes = [
-  {
-	path: '', component: Home
-  },
+  { path: '', component: Home },
+  { path:"empleados",component: EmpleadoList}, // <-- ¡Aquí están los errores!
 ];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Carrusel } from '../../components/carrusel/carrusel';
 import { Tarjetas } from '../../components/tarjetas/tarjetas';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Carrusel,Tarjetas],
+  imports: [Carrusel,Tarjetas, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
